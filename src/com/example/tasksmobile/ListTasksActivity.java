3f3e7bf05df140bffb365258de1ec2f3f71e46e6
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-04-12 22:48:07 -0300

    primeiro commit

---
+++ src/com/example/tasksmobile/ListTasksActivity.java
@@ -0,0 +1,13 @@
+package com.example.tasksmobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListTasksActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_tasks);
+	}
+}
